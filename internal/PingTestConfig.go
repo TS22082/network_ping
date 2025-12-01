a87c19414953632b7a00dc67961a5a80c9944f28
Thomas Li-Smith <ts22082@gmail.com>
2025-12-01 15:24:24 -0800

    add comments for clarity and godocs

--- internal/PingTestConfig.go
+++ internal/PingTestConfig.go
@@ -2,12 +2,18 @@ package internal
 
 import "time"
 
+// PingTestConfig holds configuration parameters for the RunTest function.
 type PingTestConfig struct {
-	Count    int
+	// Number of ICMP packets to send
+	Count int
+	// Interval between packets
 	Interval time.Duration
-	Target   string
+	// Target host to ping
+	Target string
 }
 
+// Default returns a PingTestConfig with standard default values.
+// It sets Count to 100 packets, Interval to 1 second, and Target to "www.google.com".
 func (u *PingTestConfig) Default() PingTestConfig {
 	return PingTestConfig{
 		Count:    100,
--- main.go
+++ main.go
@@ -12,29 +12,41 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Package main implements a network testing application that periodically runs ping tests
+// and reports results to the Logida API - https://logida.fly.dev/
 func main() {
+	// You will need a .env file with your LOGIDA_API_KEY set.
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Failed to load .env: %v: ", err)
 	}
 
+	// Retrieve Logida API key from environment variable. Will need to create one at https://logida.fly.dev/
 	logidaApiKey := os.Getenv("LOGIDA_API_KEY")
 	if logidaApiKey == "" {
 		fmt.Println("LOGIDA_API_KEY environment variable not set.")
 		return
 	}
 
+	// Ticker to trigger periodic tasks every 10 minutes
 	ticker := time.NewTicker(10 * time.Minute)
 	defer ticker.Stop()
 
+	// Channel to listen for interrupt signals (e.g., Ctrl+C)
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
+	// If you want to run without default setting fill in the PingTestConfig struct here,
+	// then remove the .Default() call and use the struct directly in the RunTest call
+	// below.
 	var config = internal.PingTestConfig{}
 	internal.RunTest(config.Default())
+
+	// Main event loop - runs every 10 minutes or until interrupted
 	for {
 		select {
 		case <-ticker.C:
 			fmt.Println("Performing periodic task...")
+			// if using your own config, replace config.Default() with config
 			internal.RunTest(config.Default())
 			fmt.Println("Next test schedulled in 10 minutes. Press Ctrl+C to stop.")
 		case sig := <-sigChan:

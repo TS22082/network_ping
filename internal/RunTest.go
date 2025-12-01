1e5fd750406d4d6e3e4ffbd1a871ce702369ab48
Thomas Li-Smith <ts22082@gmail.com>
2025-12-01 15:31:01 -0800

    add comments for clarity

--- internal/RunTest.go
+++ internal/RunTest.go
@@ -9,24 +9,30 @@ import (
 	probing "github.com/prometheus-community/pro-bing"
 )
 
+// RunTest performs a ping test based on the provided PingTestConfig
+// and reports the results to the Logida API.
 func RunTest(cfg PingTestConfig) {
 	target := cfg.Target
 	count := cfg.Count
 	interval := cfg.Interval
 
+	// Retrieve Logida API key from environment variable.
 	logidaApiKey := os.Getenv("LOGIDA_API_KEY")
 	if logidaApiKey == "" {
 		fmt.Println("LOGIDA_API_KEY environment variable not set.")
 		return
 	}
 
+	// Print test configuration before entering the ping loop
 	fmt.Println("Pinging", target, "with", count, "packets at", interval)
 
+	// Create a new pinger instance, the pinger is responsible for sending ICMP packets
 	pinger, err := probing.NewPinger(target)
 	if err != nil {
 		panic(err)
 	}
 
+	// Configure the pinger with parameters from PingTestConfig
 	pinger.Count = count
 	pinger.Interval = interval
 	pinger.Timeout = time.Duration(count)*interval + time.Second*5
@@ -36,10 +42,14 @@ func RunTest(cfg PingTestConfig) {
 		fmt.Printf("Sent packet #%d to %s\n", pkt.Seq, pkt.IPAddr)
 	}
 
+	// pinger.OnRecv (OnFinish, etc.) are callback functions that handle events during the ping test
+	// they dont trigger until the pinger.Run() method is called further down.
 	pinger.OnRecv = func(pkt *probing.Packet) {
 		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n", pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
 	}
 
+	// Generate a unique client ID based on current timestamp, if there is an error present
+	// the client ID can be used to track the specific log report the error happened in.
 	sixDigitRand := make([]byte, 3)
 	_, err = rand.Read(sixDigitRand)
 	if err != nil {
@@ -47,9 +57,13 @@ func RunTest(cfg PingTestConfig) {
 		return
 	}
 
+	// Convert the random bytes to a hexadecimal string
 	clientIdByTimestamp := fmt.Sprintf("%X", sixDigitRand)
 
 	pinger.OnFinish = func(stats *probing.Statistics) {
+		// Check for packet loss and report an error if any packets were lost.
+		// A log will still happen afterwards with the full statistics. They can be linked
+		// together using the client ID.
 		if stats.PacketsSent-stats.PacketsRecv > 0 {
 			error_report := map[string]interface{}{
 				"msg": "track packet loss with client_id",
@@ -67,6 +81,7 @@ func RunTest(cfg PingTestConfig) {
 			}
 		}
 
+		// Prepare the report data to be sent to Logida
 		report := map[string]interface{}{
 			"Target":        stats.Addr,
 			"IP Address":    stats.IPAddr,
@@ -92,6 +107,7 @@ func RunTest(cfg PingTestConfig) {
 		}
 	}
 
+	// Start the ping test
 	if err := pinger.Run(); err != nil {
 		panic(err)
 	}
